4d170b2f90d7f4ed7c157f589719e370b08ba63f
Andy Lang <djdreviz@gmail.com>
2022-10-02 01:12:10 +0300

    Comment, Game ja Post kontrollerit lisätty! (Get, Post, Put ja Delete)

---
+++ LFG/Model/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace LFG.Model
+{
+    public class Post
+    {        
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string CreateDate { get; set; }
+        public string Content { get; set; }
+        public string PosterProfile { get; set; }
+    }
+}
