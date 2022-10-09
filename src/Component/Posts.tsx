e3c43f511edda8fd4f2aafe5846949c9e4664ca8
Ahishi <anta@metropolia.fi>
2022-10-09 20:13:20 +0300

    Posts Edit

--- src/Component/Posts.tsx
+++ src/Component/Posts.tsx
@@ -134,46 +134,6 @@ const Posts: React.FC<Props> = ({ currentUser }) => {
                             </div>
                             <hr className='w-full border-gray-700'></hr>
                         </>
-
-                        ,{ /*
-                        <div key={Number(post.PostId)} className='flex flex-col gap-y-4'>
-                        <div key={Number(post.PostId)}>
-                            <div className='py-5 bg-primary rounded-t-md flex break-words'>
-                                <img className='w-[50px] mx-5 object-contain w-fit border-4 border-darkBackground rounded-full bg-primary' src='/images/test-avatar.png' alt='avatar' />
-                                <div className='my-auto'>
-                                    <h2 className='text-xl font-bold'>{currentUser.Nickname}</h2>
-                                    <h4 className='text-center text-sm italic font-semibold'>{post.CreateDate}</h4>
-                                </div>
-                            </div>
-                            <div className='py-5 bg-lightBackground rounded-b-lg flex'>
-                                <div className='flex flex-col justify-between h-16 px-5 mt-2'>
-                                    <button className='w-[50px] hover:text-white'>
-                                        <span className='material-symbols-outlined'>
-                                            thumb_up
-                                        </span>
-                                    </button>
-                                    <button className='w-[50px] hover:text-white'>
-                                        <span className='material-symbols-outlined'>
-                                            thumb_down
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className='pr-7'>
-                                    <h1 className='break-words text-2xl font-bold mb-2 w-fit pb-2'>{post.Title}</h1>
-                                    <p style={contentStyle}>{post.Content}</p>
-
-                                        <div className='flex pt-5'>
-                                            <p className='border-r pr-2.5'>{post.Like} likes</p>
-                                            <p className='pl-2.5'>{post.Dislike} dislikes</p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <Comment post={post}/>
-                            {currentUser.Email === email && <button onClick={() => handleDelete(post)}>Delete this post</button>}
-                            {currentUser.Email === email && <button onClick={toggle}>Edit this post</button>} {editForm && <EditPostForm currentPost={post} toggleForm={toggle} />}
-                        </div>
                     )}
                 </div>
             )
@@ -185,40 +145,41 @@ const Posts: React.FC<Props> = ({ currentUser }) => {
             <div className='flex flex-col'>
                 <hr className='w-full border-gray-700'></hr>
                 {allPosts.map(post =>
-                    <div key={Number(post.PostId)}>
-                        <div className='py-5 bg-primary rounded-t-md flex break-words'>
-                            <img className='w-[50px] mx-5 object-contain w-fit border-4 border-darkBackground rounded-full bg-primary' src='/images/test-avatar.png' alt='avatar' />
-                            <div className='my-auto'>
-                                <h2 className='text-xl font-bold hover:text-white'><Link to={`/profile/${Number(allProfiles.find(prof => Number(prof.ProfileId) === Number(post.PosterProfile))?.ProfileId)}`}>{allProfiles.find(prof => Number(prof.ProfileId) === Number(post.PosterProfile))?.Nickname}</Link></h2>
-                                <h4 className='text-center text-sm italic font-semibold'>{post.CreateDate}</h4>
-                            </div>
-                        </div>
-                        <div className='py-5 bg-lightBackground rounded-b-lg flex'>
-                            <div className='flex flex-col justify-between h-16 px-5 mt-2'>
-                                <button onClick={() => likeThis(post)} className='w-[50px] hover:text-white'>
-                                    <span className='material-symbols-outlined'>
-                                        thumb_up
-                                    </span>
-                                </button>
-                                <button onClick={() => dislikeThis(post)} className='w-[50px] hover:text-white'>
-                                    <span className='material-symbols-outlined'>
-                                        thumb_down
-                                    </span>
-                                </button>
-                            </div>
-                            <div className='pr-7'>
-                                <h1 className='break-words text-2xl font-bold mb-2 w-fit pb-2'>{post.Title}</h1>
-                                <p style={contentStyle}>{post.Content}</p>
 
-                                <div className='flex pt-5'>
-                                    <p className='border-r pr-2.5'>0 likes</p>
-                                    <p className='pl-2.5'>0 dislikes</p>
+                    <>
+                        <div key={Number(post.PostId)} className='flex rounded-lg py-5'>
+
+                            <div className='mx-5 w-fit'>
+                                <img className='w-[50px] object-contain rounded-full bg-primary' src='/images/test-avatar.png' alt='avatar' />
+                            </div>
+
+                            <div className='relative w-full flex'>
+                                <div className='w-full'>
+                                    <div className='flex h-[50px]'>
+                                        <h2 className='text-md font-bold hover:text-white'><Link to={`/profile/${Number(allProfiles.find(prof => Number(prof.ProfileId) === Number(post.PosterProfile))?.ProfileId)}`}>{allProfiles.find(prof => Number(prof.ProfileId) === Number(post.PosterProfile))?.Nickname}</Link></h2>
+                                        <h4 className='text-sm italic font-semibold text-gray-400 pt-0.5 ml-3'>{post.CreateDate}</h4>
+                                    </div>
+
+                                    <div className='mb-5'>
+                                        <h1 className='break-words text-xl font-bold mb-2'>{post.Title}</h1>
+                                        <p style={contentStyle}>{post.Content}</p>
+                                    </div>
+
+                                    <Comment post={post} />
+
+                                    <div className='flex mt-auto text-gray-400'>
+                                        <p className='border-r pr-2.5 border-gray-400'>0 likes</p>
+                                        <p className='pl-2.5'>0 dislikes</p>
+                                    </div>
                                 </div>
 
+                                <div className='w-[50px] mx-5'>
+                                </div>
                             </div>
                         </div>
-                        <Comment post={post}/>
-                    </div>
+                        <hr className='w-full border-gray-700'></hr>
+                    </>
+                     
                 )}
             </div>
         )
