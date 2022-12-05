9c98ec2d82e494d4838997237384bd534f60b3b2
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2022-12-05 14:10:50 +0200

    } x2 added

--- Model/Post.cs
+++ Model/Post.cs
@@ -5,13 +5,14 @@ using System.Diagnostics.CodeAnalysis;
 namespace LFG.Model
 {
 
-public class Post
-{
-	[Required] public Guid postId { get; set; }
-	[NotNull] [Required] public string? title { get; set; }
-	[Required] public DateTime createDate { get; set; }
-	[NotNull] [Required] public string? content { get; set; }
-	[NotNull] [Required] public string? profileId { get; set; }
-	public string? photoFileName { get; set; }
-	[Required] public int numberOfComments { get; set; }
+	public class Post
+	{
+		[Required] public Guid postId { get; set; }
+		[NotNull][Required] public string? title { get; set; }
+		[Required] public DateTime createDate { get; set; }
+		[NotNull][Required] public string? content { get; set; }
+		[NotNull][Required] public string? profileId { get; set; }
+		public string? photoFileName { get; set; }
+		[Required] public int numberOfComments { get; set; }
+	}
 }
--- Model/PostDto.cs
+++ Model/PostDto.cs
@@ -4,10 +4,11 @@ using System.Diagnostics.CodeAnalysis;
 namespace LFG.Model
 {
 
-public class PostDto
-{
-	[NotNull] [Required] public string? title { get; set; }
-	[NotNull] [Required] public string? content { get; set; }
-	public string? photoFileName { get; set; }
-	[Required] public int numberOfComments { get; set; }
+	public class PostDto
+	{
+		[NotNull][Required] public string? title { get; set; }
+		[NotNull][Required] public string? content { get; set; }
+		public string? photoFileName { get; set; }
+		[Required] public int numberOfComments { get; set; }
+	}
 }
